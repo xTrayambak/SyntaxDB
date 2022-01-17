e0fa95f570f7a762c152355e3c6797021e2f7cc6
xTrayambak <manishsowa725@gmail.com>
2022-01-17 19:42:30 +0530

    fix typos

--- syntaxdb_cmd.py
+++ syntaxdb_cmd.py
@@ -1,11 +1,9 @@
 from db import Database
 
-import pickle
-
 if __name__ == "__main__":
 
     db = Database(input("Enter database name . . . "))
     db.load()
     
     while True:
-        print(db.query(input("Enter a ScaleDB command . . . ")))
+        print(db.query(input("Enter a SyntaxDB command . . . ")))
